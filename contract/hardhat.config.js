96bb92d776f4d474b909436f6ecc83f90de07c9b
Pol <polmonteroalegre@gmail.com>
2024-09-25 13:27:36 +0200

    Update Readme, hardhat config, transaction deployed, value stored in front _> change to dot/env

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,16 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("@nomiclabs/hardhat-etherscan");
-require("dotenv").config();
+require("@nomicfoundation/hardhat-ignition");
+
 
 module.exports = {
   solidity: "0.8.17",  // or whatever version you're using
   networks: {
     holesky: {
-      url: `${process.env.HOLESKY_API_KEY}`, // replace with your Goerli RPC URL
-      accounts: [process.env.ETHSCAN_API_KEY] // Add your wallet private key here (without the "0x" prefix)
-    },
-    etherscan: {
-      apiKey: `${ethScanAPI}` // optional for contract verification
-    },
+      url: `https://eth-holesky.g.alchemy.com/v2/VX515JnRGSQ-3oUQzAzY7fERaDszAn1-`, // replace with your Goerli RPC URL
+      accounts: ["0e8ca0d61b3785b04876e2a107ec05104243d59270a0d3277856ee3d4865669e"] // Add your wallet private key here (without the "0x" prefix)
+    }
   }
 };
--- contract/test.js
+++ contract/test.js
@@ -1,5 +1,5 @@
 import { Network, Alchemy } from 'alchemy-sdk';
-
+import './.env'
 const settings = {
     apiKey: "VX515JnRGSQ-3oUQzAzY7fERaDszAn1-",
     network: Network.ETH_HOLESKY,
@@ -12,3 +12,5 @@ const usdcContract = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
 
 // Print token balances of USDC in Vitalik's address
 alchemy.core.getTokenBalances(vitalikAddress, [usdcContract]).then(console.log);
+
+console.log("your api key is _________________________________>" + "VX515JnRGSQ-3oUQzAzY7fERaDszAn1-")
