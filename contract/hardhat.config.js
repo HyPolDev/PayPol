3ca58410de00a75b1b56c8bad6e8cc66be568123
Pol <polmonteroalegre@gmail.com>
2024-09-25 12:25:37 +0200

    RECONFIG: packages -waffle +hardhattoolbox, .env... and ignition momdule to deploy

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.27",
+  solidity: "0.8.17",  // or whatever version you're using
+  networks: {
+    holesky: {
+      url: `${process.env.HOLESKY_API_KEY}`, // replace with your Goerli RPC URL
+      accounts: [process.env.ETHSCAN_API_KEY] // Add your wallet private key here (without the "0x" prefix)
+    },
+    etherscan: {
+      apiKey: `${ethScanAPI}` // optional for contract verification
+    },
+  }
 };
